e2127d6de83eee2a4a09960d694987ae42ffdb0c
Fabydanto79 <132086483+Fabydanto79@users.noreply.github.com>
2025-10-16 22:44:56 +0200

    Remove Firebase SDK setup from script.js
    Removed Firebase SDK import and initialization code.

--- script.js
+++ script.js
@@ -213,25 +213,3 @@
   window._gb = { products, expirations, sales };
 
 })();
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBGPcIf_PsIwoUSwgD7-AOjl0Y-N83b8YE",
-  authDomain: "gestionalebar.firebaseapp.com",
-  projectId: "gestionalebar",
-  storageBucket: "gestionalebar.firebasestorage.app",
-  messagingSenderId: "189700423445",
-  appId: "1:189700423445:web:3d850fe9ac50ae1a8c331c",
-  measurementId: "G-VSZB6RZGYE"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
